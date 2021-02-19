13d0dd88b7c85816ee5d875da29be5fbed55861b
Colin Baxter <colin.baxter@zaneray.com>
2021-02-19 00:46:58 -0700

    No more buttons

--- pages/index.vue
+++ pages/index.vue
@@ -5,24 +5,6 @@
       <h1 class="title">
         {{ page.title }}
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
