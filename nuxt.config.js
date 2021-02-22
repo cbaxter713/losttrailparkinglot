47551a169c00d4df7849900e9602f41f9893960f
Colin Baxter <colin.baxter@zaneray.com>
2021-02-22 09:49:00 -0700

    Cleaning up dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -136,10 +136,7 @@ export default {
   },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['@nuxtjs/style-resources', '@nuxtjs/axios', '@nuxtjs/pwa', '@nuxtjs/gtm'],
